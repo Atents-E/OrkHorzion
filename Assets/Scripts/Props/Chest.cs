4106b196fa8a216084f8081b9dcc0295a368ab56
audrms259 <111333481+audrms259@users.noreply.github.com>
2022-10-03 15:43:24 +0900

    보물상자 애니메이션 구현

---
+++ Assets/Scripts/Props/Chest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    // 애니메이터 선언
+    Animator anim;
+
+    private void Awake()
+    {
+        // 애니메이터 추가
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // "Player"라는 태그가 닿으면
+        if (other.CompareTag("Player"))
+        {
+            // Open 애니메이션 활성화
+            anim.SetTrigger("Open");
+
+            // 활성화 후 0.5초 후 사라짐
+            Destroy(this.gameObject, 1.3f);
+        }
+    }
+
+
+
+}
