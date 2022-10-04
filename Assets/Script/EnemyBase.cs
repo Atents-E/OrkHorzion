ec7286df237e28b24b5a069de8d584384c0c633c
JM991103 <ljn1103@naver.com>
2022-10-04 16:45:10 +0900

    오크 중간 점검
    중간 점검

--- Assets/Script/EnemyBase.cs
+++ Assets/Script/EnemyBase.cs
@@ -2,17 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float moveSpeed = 1.0f;
+    public int monsterHp = 100;
+
+    public float AttackDemage = 10.0f;
+    public float AttackDelay = 5.0f;
+
+    Rigidbody rigid;
+
+    Animator anim;
+    
+    bool isMove = false;
+
+    public int MonsterHP
     {
-        
+        get => monsterHp;
+        set
+        {
+            monsterHp = value;
+
+            if (monsterHp < 0)
+            { 
+                Die();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
+
+    private void Start()
+    {
+        AttackFalse();
+        isMove = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isMove)
+        {
+            rigid.MovePosition(transform.position + moveSpeed * Time.fixedDeltaTime * transform.forward);
+            MonsterHP -= 10;
+        }
+        else
+        {
+           
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            AttackTrue();
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            AttackFalse();
+        }
+    }
+
+    void Die()
+    {
+        anim.SetTrigger("Die");
+        isMove = true;
+        Destroy(gameObject,2.0f);
+    }
+
+    void AttackTrue()
+    {
+        isMove = true;
+        anim.SetBool("Attack",true);
+    }
+    void AttackFalse()
+    {
+        isMove = false;
+        anim.SetBool("Attack", false);
+    }
+
 }
