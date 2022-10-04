73ef2e41ec75501ab208299c3983c900dcd35fed
audrms259 <111333481+audrms259@users.noreply.github.com>
2022-10-04 14:54:23 +0900

    보물상자 오픈시 이펙트 추가

--- Assets/Scripts/Props/Chest.cs
+++ Assets/Scripts/Props/Chest.cs
@@ -6,11 +6,13 @@ public class Chest : MonoBehaviour
 {
     // 애니메이터 선언
     Animator anim;
+    ParticleSystem particle;
 
     private void Awake()
     {
         // 애니메이터 추가
         anim = GetComponent<Animator>();
+        particle = GetComponentInChildren<ParticleSystem>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,6 +22,7 @@ public class Chest : MonoBehaviour
         {
             // Open 애니메이션 활성화
             anim.SetTrigger("Open");
+            particle.Play();
 
             // 활성화 후 0.5초 후 사라짐
             Destroy(this.gameObject, 1.3f);
