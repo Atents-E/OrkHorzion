803286a9e5b912438babf6236baeb0d0a2c15c0c
audrms259 <111333481+audrms259@users.noreply.github.com>
2022-10-07 14:51:13 +0900

    오류 삭제

--- Assets/Scripts/RelicAtk.cs
+++ Assets/Scripts/RelicAtk.cs
@@ -6,15 +6,9 @@ public class RelicAtk : Relic
 {
     string[] relicsName = { "으악", "우엑", "뜨악" };
 
-    override void GenerateRelics(relicsName[0],1,true)
-    { 
-        
-    };
-
 
     private void Start()
     {
-        Debug.Log($"{relicName}");
     }
 
 }
