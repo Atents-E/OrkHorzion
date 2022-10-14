cac338e421ea8641167fe53f5fd9b01f3d4e9bec
JM991103 <ljn1103@naver.com>
2022-10-14 15:05:39 +0900

    몬스터 플레이어 타겟

--- Assets/Script/Monster/EnemyBase.cs
+++ Assets/Script/Monster/EnemyBase.cs
@@ -13,11 +13,11 @@ public class EnemyBase : MonoBehaviour
     public float AttackDemage = 10.0f;  // 몬스터가 공격할때의 데미지
     public float AttackDelay = 5.0f;    // 몬스터가 공격한 뒤 다음 공격까지의 텀
     public float turnSpeed = 5.0f;      // 몬스터가 회전하는 속도 
-    float currentAngle = 0.0f;          // 초당 바뀌는 각도
+    float currentAngle = 5.0f;          // 초당 바뀌는 각도
 
     Transform playerTarget = null;       // 플레이어가 없다
 
-    Vector3 monsterToplayerDir;          // 몬스터와 플레이어 사이의 거리
+    //Vector3 monsterToplayerDir;          // 몬스터와 플레이어 사이의 거리
 
     bool looktargetOn = false;            // 몬스터가 플레이어를 바라보는지 
 
@@ -27,8 +27,8 @@ public class EnemyBase : MonoBehaviour
     
     Animator anim;
 
-    float random;
-    public float Random { get => UnityEngine.Random.Range(0, 1); }      // 어택2용 랜덤값 할당
+    //float random;
+    //public float Random { get => UnityEngine.Random.Range(0, 1); }      // 어택2용 랜덤값 할당
 
     public int MonsterHP        // HP 프로퍼티
     {
@@ -40,6 +40,7 @@ public class EnemyBase : MonoBehaviour
             if (monsterHp < 0)
             {
                 monsterHp = 0;
+
                 Die();
             }
         }
@@ -55,6 +56,7 @@ public class EnemyBase : MonoBehaviour
     {
         //SphereCollider coll = GetComponent<SphereCollider>();
         //coll.radius = AttackRadius;
+        
     }
 
     private void Update()
@@ -63,7 +65,7 @@ public class EnemyBase : MonoBehaviour
 
         if (looktargetOn)
         {
-            LookTarget();
+            Looktarget();
         }
 
         MonsterHP -= 10;
@@ -75,7 +77,6 @@ public class EnemyBase : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("들어옴");
-            anim.SetTrigger("Attack");
             looktargetOn = true;
             playerTarget = other.transform;         // playerTarget이 null이 아니게 되었다.
             OffMoving();
@@ -88,6 +89,7 @@ public class EnemyBase : MonoBehaviour
         {
             Debug.Log("나감");
             OnMoving();
+
             looktargetOn = false;
             playerTarget = null;                    // playerTarget이 null이 되었다.
             
@@ -96,57 +98,76 @@ public class EnemyBase : MonoBehaviour
 
     void OnMoving()
     {
+        //agent.updatePosition = true;
         agent.isStopped = false;
-        
+        anim.SetBool("Idle", false);
+        anim.SetBool("Attack", false);
+
     }
 
     void OffMoving()
     {
         //isMoving = false;
+        anim.SetBool("Idle",true);
+        anim.SetBool("Attack",true);
         agent.isStopped = true;
-        
-
+        //agent.updatePosition = false;
     }
 
     void Die()
     {
         anim.SetTrigger("Die");
+        agent.isStopped = true;
+        looktargetOn = false;
         Destroy(gameObject, 3.0f);  // 3초뒤에 몬스터 오브젝트 삭제    
     }
 
-    void LookTarget()
+    void Looktarget()
     {
         if (looktargetOn)
         {
             if (playerTarget != null)
             {
-                monsterToplayerDir = playerTarget.position - transform.position;
-                monsterToplayerDir.y = 0;
+                Vector3 dir = (playerTarget.position - transform.position).normalized;
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), currentAngle * Time.deltaTime);
 
-                float betweenAngle = Vector3.SignedAngle(transform.forward, monsterToplayerDir, transform.up);
-
-                Vector3 resultDir;
-
-                if (Mathf.Abs(betweenAngle) < 10.0f)
-                {
-                    float rotateDir = 1.0f;
-
-                    if (betweenAngle < 0)
-                    {
-                        rotateDir = -1.0f;
-                    }
-
-                    currentAngle += (rotateDir * turnSpeed * Time.fixedDeltaTime);
-
-                    resultDir = Quaternion.Euler(0, currentAngle, 0) * transform.forward;
-                }
-                else
-                {
-                    resultDir = monsterToplayerDir;
-                }
-                transform.rotation = Quaternion.LookRotation(resultDir);
             }
-
         }
     }
+
+    //void LookTarget()       // 플레이어 방향 바라보기
+    //{
+    //    if (looktargetOn)
+    //    {
+    //        if (playerTarget != null)
+    //        {
+    //            monsterToplayerDir = playerTarget.position - transform.position;
+    //            monsterToplayerDir.y = 0;
+
+    //            float betweenAngle = Vector3.SignedAngle(transform.forward, monsterToplayerDir, transform.up);
+
+    //            Vector3 resultDir;
+
+    //            if (Mathf.Abs(betweenAngle) < 10.0f)
+    //            {
+    //                float rotateDir = 1.0f;
+
+    //                if (betweenAngle < 0)
+    //                {
+    //                    rotateDir = -1.0f;
+    //                }
+
+    //                currentAngle += (rotateDir * turnSpeed * Time.fixedDeltaTime);
+
+    //                resultDir = Quaternion.Euler(0, currentAngle, 0) * transform.forward;
+    //            }
+    //            else
+    //            {
+    //                resultDir = monsterToplayerDir;
+    //            }
+    //            transform.rotation = Quaternion.LookRotation(resultDir);
+    //        }
+
+    //    }
+    //}
 }
