a3ec42d4a5dfff3352d3111ccd0c475093630073
Wi-jisu <111379239+Wi-jisu@users.noreply.github.com>
2023-01-05 17:55:37 +0900

    GameManager, Singleton

---
+++ Assets/Script/Player/Core/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    Character character;
+    Warrior warrior;
+    Wizard wizard;
+   
+    public Character Character => character;
+    public Warrior Warrior => warrior;   
+    public Wizard Wizard => wizard;
+
+    protected override void Initialize()
+    {
+       character = FindObjectOfType<Character>();
+        warrior = FindObjectOfType<Warrior>();
+        wizard = FindObjectOfType<Wizard>();
+    }
+}
