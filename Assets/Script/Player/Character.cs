5e2682527db00e6b6f8b2ceb655edd3f787cefb9
Wi-jisu <111379239+Wi-jisu@users.noreply.github.com>
2023-01-05 14:19:02 +0900

    Character 스크립트 업데이트

--- Assets/Script/Player/Character.cs
+++ Assets/Script/Player/Character.cs
@@ -1,100 +1,149 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.ParticleSystem;
+using Unity.VisualScripting;
+using Random = UnityEngine.Random;
 
-public class Character : MonoBehaviour
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Character : MonoBehaviour, IBattle, IHealth
 {
+    public GameObject point;
+    public ParticleSystem spawnPS;
+    
+
     protected string characterName; // 캐릭터 이름
-    public string CharacterName // 캐릭터 이름 프로퍼티
-    {
-        get => CharacterName;
-
-        set
-        {
-            CharacterName = value;
-        }
-    }
-
-    protected bool isDead = false; // 캐릭터 사망 확인용
-
-    protected int hp; // 캐릭터 체력
-    public int HP // 캐릭터 체력 프로퍼티
-    {
-        get => hp;
-
-        set
-        {
-            hp = value;
-            if (hp > maxHp) // 현재 체력이 최대체력 보다 높아지면 ex) 회복할 때
-            {
-                hp = maxHp; // 현재 체력을 최대체력과 같게 만들기
-            }
-            if (hp <= 0) // 현재 체력이 0 이거나 0 이하힐 때 ex) 현재 체력보다 더 큰 데미지를 입을 때
-            {
-                hp = 0; // 체력을 0으로 만들기
-            }
-        }
-    }
-
-    protected int maxHp; // 캐릭터 최대 체력
-    public int MAXHP // 캐릭터 최대 체력 프로퍼티
-    {
-        get => maxHp;
-        set
-        {
-            // 나중에 유물효과로 최대체력 늘릴 때 쓰는 공간
-            maxHp = value;
-        }
-    }
-
-    protected int def; // 캐릭터 방어력
-    public int DEF // 캐릭터 방어력 프로퍼티
-    {
-        get => def;
-
-        set
-        {
-            def = value;
-        }
-    }
-
-    protected int atk; // 캐릭터 공격력
-    public int ATK // 캐릭터 공격력 프로퍼티
-    {
-        get => atk;
-
-        set
-        {
-            atk = value;
-        }
-    }
-
-    protected float atkSpeed; // 캐릭터 공격속도
-    public float ATKSpeed // 캐릭터 공격속도 프로퍼티
-    {
-        get => atkSpeed;
-
-        set
-        {
-            atkSpeed = value;
-        }
-    }
-
+    protected float atk; // 캐릭터 공격력
+    protected float def; // 캐릭터 방어력
+    protected float hp; // 캐릭터 체력
+    protected float maxHp; // 캐릭터 최대 체력
+    protected float criticalChance; // 캐릭터 치명타 확률
     protected float moveSpeed; // 캐릭터 이동속도
-    public float MoveSpeed // 캐릭터 이동속도 프로퍼티
-    {
-        get => moveSpeed;
 
-        set
+    public bool isAlive = true;
+
+    Animator anim;
+
+
+    // 프로퍼티-----------------------------------------------------------------------------------------------------------
+    public string CharacterName => characterName; // 캐릭터 이름 프로퍼티
+
+    public float MaxHP => maxHp;  //캐릭터 최대 체력 프로퍼티
+
+    public float DEF => def; // 캐릭터 방어력 프로퍼티
+
+    public float ATK => atk; // 캐릭터 공격력 프로퍼티
+
+    public float CriticalChance => criticalChance; // 캐릭터 치명타 확률 프로퍼티
+
+    public float MoveSpeed => moveSpeed; // 캐릭터 이동속도 프로퍼티
+
+    public float HP 
+    { 
+        get => hp;
+        set { 
+            if(hp != value)
+            { 
+                hp = value;
+               
+                if (hp <= 0)
+                {
+                    Die();
+                }
+                
+                hp = Mathf.Clamp(hp, 0.0f, maxHp);
+
+                onHealthChange?.Invoke(hp/maxHp);
+            }
+            
+        } 
+    }
+
+
+    // 델리게이트 --------------------------------------------------------------------------------------------
+    
+    public Action <float> onHealthChange { get; set; }
+    
+    public Action onDie { get; set; }
+    // --------------------------------------------------------------------------------------------------------
+
+    protected virtual void Awake()
+    {
+        anim = GetComponent<Animator>();
+        point = GameObject.Find("SpawnPoint");
+        spawnPS = point.GetComponentInChildren<ParticleSystem>();
+    }
+
+    protected virtual void Start()
+    {
+        hp = maxHp;
+        isAlive = true;
+        
+    }
+
+    public virtual void Attack(IBattle target)
+    {
+        float damage = ATK; // 데미지 = 해당 클래스의 공격력
+        if (Random.Range(0.0f, 1.0f) < criticalChance)
         {
-            moveSpeed = value;
+            Debug.Log("Critical★");
+            damage *= 1.5f;
+              
+        }
+        target?.Defence(damage);// 플레이어는 공격력만큼 데미지를 주지만 치명타가 뜨면 1.5배의 데미지를 준다.
+    }
+
+    public virtual void Defence(float damage)
+    {
+        if (isAlive)                // 살아있을 때만 데미지 입음.
+        {
+            anim.SetTrigger("Hit"); // 피격 애니메이션 재생            
+            float finalDamage = damage * (1.0f - DEF / (DEF + 100.0f));
+            HP -= finalDamage;
+            Debug.Log($"{gameObject.name}의 HP가 {HP}로 변경되었습니다.");
         }
     }
 
-    private void Dead()
+    public virtual void Die()
     {
-        isDead = true;
+            Debug.Log($"{gameObject.name}가 죽었습니다");
+            isAlive = false;
+            anim.SetBool("IsAlive", isAlive);   // 죽었다고 표시해서 사망 애니메이션 재생
+            onDie?.Invoke();
+            Invoke("Recover", 3f);
+
     }
 
+    public virtual void Recover()
+    {
+        bool on = true;
+        if (spawnPS != null)
+        {
+            if (on)
+            {
+                spawnPS.Play();
+            }
+            else
+            {
+                spawnPS.Stop();
+            }
+        }
+        isAlive = true;
+        anim.SetBool("IsAlive", isAlive);
+        anim.SetTrigger("Recover");
+
+        HP = maxHp;
+
+        Vector3 spotPos = point.transform.position;
+        transform.position = spotPos;
+        Debug.Log($"{gameObject.name}가 다시 살아납니다");
+       
+        
+    }
 
 }
