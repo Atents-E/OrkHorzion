0e2f0ef7c52f94120154d4f9f9ad686b67e3acd5
lse2006 <111333404+lse2006@users.noreply.github.com>
2023-01-11 16:36:34 +0900

    Gold.cs_작성 중

---
+++ Assets/Eunji/Scripts/PlayerGold.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+// PlayerGold의 기능
+// 1. 몬스터가 죽으면 드랍되는 모든 골드의 금액을 저장
+// 2. 골드 금액이 변경될 때마다 UI의 골드 금액도 변경되어 표시  
+
+public class PlayerGold : MonoBehaviour
+{
+    public float maxGold = 10000000000;     // 최소 금액
+    public float nowGold = 100;             // 현재 금액
+
+    TextMeshProUGUI gold_Text;              // 텍스트로 표시 할 골드 금액
+    Action OnGoldChange;                    // 델리게이트로 골드 소유량 변경 확인
+
+    public float NowGold
+    {
+        get => nowGold;
+        set
+        {
+            if(nowGold != value)
+            {
+                NowGold -= value;
+                nowGold = Mathf.Clamp(0, value, maxGold);
+
+                OnGoldChange?.Invoke(); 
+            }
+        }
+    }
+
+
+    private void Awake()
+    {
+        gold_Text = GetComponent<TextMeshProUGUI>();
+        OnGoldChange += GoldChange;
+
+        gold_Text.text = $"{NowGold:f0}";             // 골드 금액 표시
+    }
+
+    /// <summary>
+    /// 변경 된 금액으로 UI 숫자 변경
+    /// </summary>
+    private void GoldChange()
+    {
+        gold_Text.text = $"{NowGold:f0}";             // 골드 금액 표시
+    }
+}
