81fe7a7a9bd5876f0b98ae8849121f7e6f7864f1
audrms259 <111333481+audrms259@users.noreply.github.com>
2023-01-19 14:23:46 +0900

    asd

--- Assets/JangMyeongGeun/Scripts/Test/Test_ShowPlayerStat.cs
+++ Assets/JangMyeongGeun/Scripts/Test/Test_ShowPlayerStat.cs
@@ -14,7 +14,7 @@ public class Test_ShowPlayerStat : MonoBehaviour
         stats = GetComponentsInChildren<TextMeshProUGUI>();
         stats[1].text = $"MAXHP : {player.MaxHP}";
         stats[2].text = $"Atk : {player.AttackPower}";
-        stats[3].text = $"Critical : {player.CriticalChance}";
+        stats[3].text = $"Critical % : {player.CriticalChance * 100:F0}%";
         stats[4].text = $"Def : {player.DefencePower}";
         stats[5].text = $"MoveSpeed : {player.MoveSpeed}";
     }
@@ -24,7 +24,7 @@ public class Test_ShowPlayerStat : MonoBehaviour
     {
         stats[1].text = $"MaxHP : {player.MaxHP}";
         stats[2].text = $"Atk : {player.AttackPower}";
-        stats[3].text = $"Critical : {player.CriticalChance}";
+        stats[3].text = $"Critical % : {player.CriticalChance * 100:F0}%";
         stats[4].text = $"Def : {player.DefencePower}";
         stats[5].text = $"MoveSpeed : {player.MoveSpeed}";
     }
